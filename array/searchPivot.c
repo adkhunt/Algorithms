8ddf0254d7f8cd876da3882c3d7278a66c780cdf
ADKhunt <akash.kumar@digicollect.com>
2017-01-23 18:39:58 +0530

    #

--- array/searchPivot.c
+++ array/searchPivot.c
@@ -1,5 +1,39 @@
 #include"header.h"
 
+/////////////////////////////////////////////
+//					   //
+// Program to search pivot index in sorted //
+// Rotated array in O(log n) time	   //
+//					   //
+/////////////////////////////////////////////
+
+int getPivot(int *a,int start,int end)
+{
+	if(start > end)
+		return -1;
+
+	if(start == end)
+		return start;
+
+	int mid = (start + end)/2;
+
+	if(mid < end && a[mid] > a[mid+1])
+		return mid;
+
+	if(mid > start && a[mid] < a[mid-1])
+		return mid-1;
+
+	if(a[start] >= a[mid])
+		return getPivot(a,start,mid-1);
+
+	return getPivot(a,mid+1,end);
+}
+
+int searchPivot(int *a,int size)
+{
+	return getPivot(a,0,size);
+}
+
 int main()
 {
 int *arr = null;
@@ -15,6 +49,8 @@ int size,i;
 		scanf("%d",&arr[i]);
 
 	int pivot = searchPivot(arr,size);
+
+	printf("Pivot index = %d\n",pivot);
 
 	return EXIT_SUCCESS;
 }
