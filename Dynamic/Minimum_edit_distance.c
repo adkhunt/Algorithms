a27c930b8855f92d9d104e12c01a28bc82a5fce9
ADKhunt <akash.kumar@digicollect.com>
2017-03-04 20:45:35 +0530

    #

--- Dynamic/Minimum_edit_distance.c
+++ Dynamic/Minimum_edit_distance.c
@@ -26,7 +26,7 @@ int minimum_edit_distance(char *str1,char *str2)
 {
 int len1 = my_strlen(str1);
 int len2 = my_strlen(str2);
-int mat[len1][len2],i,j;
+int mat[len1+1][len2+1],i,j;
 
 	for(i = 0 ; i <= len1 ; ++i)
 		mat[i][0] = i;
@@ -34,19 +34,19 @@ int mat[len1][len2],i,j;
 	for(i = 0 ; i <= len2 ; ++i)
 		mat[0][i] = i;
 
-	for(i = 1 ; str1[i] ; ++i)
+	for(i = 1 ; i<=len1 ; ++i)
 	{
-		for(j = 1 ; str2[j] ; ++j)
+		for(j = 1 ; j<=len2 ; ++j)
 		{
 			if(str1[i-1] != str2[j-1])
 				mat[i][j] = min(mat[i-1][j-1],mat[i-1][j],mat[i][j-1]) + 1;
 
 			else
-				mat[i][j] = min(mat[i-1][j-1],mat[i-1][j],mat[i][j-1]);
+				mat[i][j] = mat[i-1][j-1];
 		}
 	}
 
-	return mat[len1-1][len2-1];
+	return mat[len1][len2];
 }
 
 char* get_string(void)
